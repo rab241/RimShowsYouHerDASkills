f0f1c811324065de79e76bdc276e375d9cb653fc
rab241 <84033599+rab241@users.noreply.github.com>
2021-06-30 11:56:26 -0400

    Update WHProjectFinal.py

--- WHProjectFinal.py
+++ WHProjectFinal.py
@@ -10,24 +10,24 @@ df.describe()
 #since all columns have same number of rows, no need to deal with missing rows/uneven columns
 
 
-
 #In the data, the countries within each region have average ladder scores measuring each country's happiness
 #It would be easy to rank by ladder score and decide to go to happiest country; however, the ladder score is made up of several factors.
-#As a single traveller and woman, I take being free and having social support as an essential. Generally leads to safety and being able to go around wherever I need on my own
+#As a single traveller and woman, I take being free and having social support as an essential. Generally leads to safety in being able to depend on others and do as I please.
+
 #With my southern hospitality, I love the generosity and welcoming feel of people within a new country.
-#For social support, generosity, and freedom to make life choices factors calculated into the ladder score. I want those to be a high weight of the ladder score.
+#Social support, generosity, and freedom to make life choices factors are weighted into the ladder score. 
+#I want to see countries who prioritize such factors
 df2 = df[(df["social_support"]>=0.80) & (df["freedom_to_make_life_choices"]) & (df["generosity"]>=0.15) ]
 df2
 #since 9 countries in 4 seperate regions come out with initial priorities, I want to know the best country to go to within each region. 
 
 
-
-#The best is the higher ladder score and smaller std dev of ladder score, if the larger ladder score has a larger std dev, then actually pick the one with next largest
+#The best is the higher ladder score and smallest std dev of ladder score, if the larger ladder score has a larger std dev, then actually pick the one with next largest
 df2.groupby(["regional_indicator" ,"country_name"]).agg(min_stldsc = pd.NamedAgg(column = "standard_error_of_ladder_score", aggfunc = 'min'), max_ladsc = pd.NamedAgg(column = "ladder_score", aggfunc = 'max'))
 #Specific country for each region: Kosovo for Central and Eastern Europe, Turkmenistan for CIS, Australia for North America and ANZ, Indonesia for Southeast Asia, and Iceland for Western Europe
 
 
-#to gain more insight into the extra attributes, use box and whisker plots, and horizontal bar plots
+#To gain more insight into the extra attributes, use box and whisker plots, and horizontal bar plots
 import matplotlib.pyplot as plt
 #I like the number of choices in a region that I might decide to go to one and have a road trip
 df_country_region = df2.groupby("regional_indicator").country_name.count()
@@ -37,8 +37,6 @@ plt.xlabel("Regions")
 plt.ylabel("Number of Countries")
 #Because Western Europe has the most regions I can explore, I would go to Western Europe
 
-    
-![png](output_5_1.png)
     
 # To be sure, I want to check if Western Europe's ladder score is top three mean score
 region_ladder_mean = df2.groupby("regional_indicator").ladder_score.mean()
@@ -52,7 +50,6 @@ plt.ylabel("Mean Happiness Score")
 #Officially, I will travel to Western Europe and start in Iceland 
 
 
-![png](output_6_1.png)
     
 
 
